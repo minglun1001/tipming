e562f735d9861ae88f751a275a3d1e1577b3e1b2
minglun1001 <mingking0324@gmail.com>
2026-07-18 14:13:10 +0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>TipMing ❤️</h1>
+      <p>Thanks for supporting my content!</p >
+
+      <button>Touch 'n Go</button>
+      <button>DuitNow</button>
+      <button>PayPal</button>
+    </main>
+  );
+}
